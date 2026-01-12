39cedbac3472630789abb2928ffc495e659ac62e
Mahmud <mahxgithub@gmail.com>
2026-01-12 22:43:25 +0100

    Changed the "Resumind" text on the navbar to "ResuMind"

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router"
 export default function Navbar() {
     return (
         <nav className="navbar">
-            <Link to="/" className="text-2xl font-bold text-gradient"><p>Resumind</p></Link>
+            <Link to="/" className="text-2xl font-bold text-gradient"><p>ResuMind</p></Link>
             <Link to="/upload" className="primary-button w-fit"><p>Upload Resume</p></Link>
         </nav>
     );
