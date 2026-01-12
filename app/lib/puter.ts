f00cab750482a9c63131fab3a1aec8a106234a75
Mahmud <mahxgithub@gmail.com>
2026-01-12 23:06:14 +0100

    Changed the model to "claude-haiku-4.5"

--- app/lib/puter.ts
+++ app/lib/puter.ts
@@ -350,7 +350,7 @@ export const usePuterStore = create<PuterStore>((set, get) => {
           ],
         },
       ],
-      { model: "claude-haiku-4" }
+      { model: "claude-haiku-4.5" }
     ) as Promise<AIResponse | undefined>;
   };
 
